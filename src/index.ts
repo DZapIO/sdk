51d425818fb6f5ce507c4fd8b5539014b6e788b7
Nelson drew <nelsondrew07@gmail.com>
2023-11-10 16:26:08 +0530

    Testing env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import useClient from "./hooks/useClient";
 import useContract from "./hooks/useContract";
+// Test Scripts
+// import TestModules from "./test";
+
+
+// TestModules();
+
 export { useClient, useContract };
