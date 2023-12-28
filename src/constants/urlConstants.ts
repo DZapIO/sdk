fa1d537561b082a1235ecc5514e14f3dc43eb3bf
dzaptech <dezaptech@gmail.com>
2023-12-28 20:38:59 +0530

    Clean SDK code
    Clean SDK code and add scroll chain

---
+++ src/constants/urlConstants.ts
@@ -0,0 +1,7 @@
+// Batch swap urls
+export const BATCH_SWAP_QUOTE_URL = 'swap/quote';
+export const BATCH_SWAP_PARAMS_URL = 'swap/params';
+export const BATCH_SWAP_SUPPORTED_CHAINS_URL = 'config/supported-chains';
+
+// Common URLS
+export const GET_ALL_TOKENS_URL = 'token/tokens';
