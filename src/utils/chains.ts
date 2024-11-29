62ab12cdd06924d674621295e8fc1bf16104aa71
kiranchaudhary <kiranchaudhary537@gmail.com>
2024-11-29 17:19:46 +0530

    Integration of custom chains

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -1,3 +1,4 @@
+import { defineChain } from 'viem';
 import * as viemChains from 'viem/chains';
 
 // @dev kept for reference
@@ -29,7 +30,57 @@ import * as viemChains from 'viem/chains';
 //   sourceId: 1,
 // });
 
-export const viemChainsById: Record<number, viemChains.Chain> = Object.values(viemChains).reduce((acc, chainData) => {
+const fiveIre = /*#__PURE__*/ defineChain({
+  id: 995,
+  name: '5ire',
+  nativeCurrency: {
+    decimals: 18,
+    name: '5ire',
+    symbol: '5ire',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.5ire.network'] },
+  },
+  blockExplorers: {
+    default: {
+      name: '5ire Scan',
+      url: 'https://5irescan.io',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 47416,
+    },
+  },
+});
+
+const arthera = /*#__PURE__*/ defineChain({
+  id: 10242,
+  name: 'Arthera',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'AA',
+    symbol: 'AA',
+  },
+  rpcUrls: {
+    default: { http: ['https://rpc.arthera.net'] },
+  },
+  blockExplorers: {
+    default: {
+      name: 'Arthera Scan',
+      url: 'https://explorer.arthera.net',
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 4502791,
+    },
+  },
+});
+
+export const viemChainsById: Record<number, viemChains.Chain> = [...Object.values(viemChains), fiveIre, arthera].reduce((acc, chainData) => {
   return chainData.id
     ? {
         ...acc,
