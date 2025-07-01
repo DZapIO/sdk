08548420043720ea1951afe683296cf7e0179a1e
Manraj <65106261+manraj2712@users.noreply.github.com>
2025-07-01 16:28:17 +0530

    update signature deadline to 30mins

--- src/constants/permit2.ts
+++ src/constants/permit2.ts
@@ -2,4 +2,4 @@ import { maxUint48 } from 'viem';
 
 export const MaxAllowanceTransferAmount = maxUint48;
 export const MaxAllowanceExpiration = maxUint48;
-export const SignatureExpiryInSecs = 604800; // 7 days
+export const SignatureExpiryInSecs = 1800; // 30 minutes
