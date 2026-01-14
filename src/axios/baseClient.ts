70309edadf110fcd7da1d43bfe71a6c018a63b87
prajjwaljogdand <prajjwaljogdand@gmail.com>
2026-01-14 20:30:16 +0530

    merge eslint updates

--- src/axios/baseClient.ts
+++
@@ -1,6 +0,0 @@
-import { config } from '../config';
-import AxiosClient from '.';
-
-export const baseApiClient = AxiosClient.getInstance(config.getBaseUrl());
-
-export const baseZapApiClient = AxiosClient.getInstance(config.getBaseZapUrl());
