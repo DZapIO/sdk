55eadab5e341a596b43f2dce1f91c96af0615852
prajjwaljogdand <prajjwaljogdand@gmail.com>
2026-01-21 00:39:07 +0530

    build fix

--- src/service/trade/index.ts
+++ src/service/trade/index.ts
@@ -14,6 +14,7 @@ import type {
   BroadcastTxParams,
   BroadcastTxResponse,
   CalculatePointsRequest,
+  ChainData,
   ContractErrorResponse,
   DZapTransactionResponse,
   GaslessTradeBuildTxnResponse,
