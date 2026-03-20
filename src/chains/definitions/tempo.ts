09e8c26892f50cd22da948fa069ca178ad443039
Shoydon Alphonso <info.shoydon@gmail.com>
2026-03-20 19:58:04 +0530

    update: add missing multicall address in tempo chain definition

--- src/chains/definitions/tempo.ts
+++ src/chains/definitions/tempo.ts
@@ -1,6 +1,11 @@
 import { tempo as tempoViem } from 'viem/chains';
-import { tempoNativeToken } from '../../constants/address';
 
 export const tempo = tempoViem.extend({
-  feeToken: tempoNativeToken,
+  feeToken: '0x20C0000000000000000000000000000000000000',
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 775442,
+    },
+  },
 });
