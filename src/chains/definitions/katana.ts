a73ce5431207008bf8f2f57eaf995354dda2be95
Shoydon Alphonso <info.shoydon@gmail.com>
2026-09-03 20:57:09 +0530

    feat: export custom viem and bigmi chains

---
+++ src/chains/definitions/katana.ts
@@ -0,0 +1,11 @@
+import { katana as Katana } from 'viem/chains';
+import { extendViemChain } from '../../utils/extendViemChain';
+
+export const katana = extendViemChain(Katana, {
+  contracts: {
+    multicall3: {
+      address: '0xcA11bde05977b3631167028862bE2a173976CA11',
+      blockCreated: 1898013,
+    },
+  },
+});
